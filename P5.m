383ef25d35dbbc2670df19ce4244652c9a84ee3e
xamenss <137056002+xamenss@users.noreply.github.com>
2025-01-22 14:42:40 +0100

    P5 pas fini, help me

---
+++ P5.m
@@ -0,0 +1,49 @@
+[im_flou, map_flou]=imread('texte_flou.png');   % Lecture de l'image flou
+[im_ref, map_ref]=imread('texte_ref.png');   % Lecture de l'image flou
+l=size(im,1);                        % Hauteur image initiale
+c=size(im,2);                        % Largeur image initiale
+K_flou = 11;
+
+D=fftshift(fft2(im));               % TF image
+H = zeros(l,c);                        % Matrice de filtrage
+IM_flou_simple = zeros(l,c);           % image rétablie avec une simple inversion sans tenir compte des bruits 
+
+
+
+% Affichage image flou
+figure('numbertitle','off','name','Image flou');
+image(im_flou);
+colormap(map_flou);
+
+% Affichage image réference
+figure('numbertitle','off','name','Image reference');
+image(im_ref);
+colormap(map_ref);
+
+
+% Créer la réponse impulsionnelle (h)
+h = zeros(l, c);
+h(floor(l/2)-1:floor(l/2)+1, floor(c/2)-K_flou/2:floor(c/2)+K_flou/2) = 1;
+
+h_norm = normalize(h);
+
+% Calcul de la TF de H
+H = fftshift(fft2(h_norm));
+
+% TF de H
+figure('numbertitle', 'off', 'name', 'TF de H');
+plot(H);
+title('H');
+grid on;
+
+% Filtrage Inverse simple
+IM_flou_simple = D ./ H;
+
+% Passage à l'image "visuelle"
+im_flou_simple = real(ifft2(fftshift(IM_flou_simple)));
+
+
+% Affichage image flou avec le filtrage inverse simple
+figure('numbertitle','off','name','Image flou avec le filtrage inverse simple');
+image(im_flou_simple);
+colormap(map_flou);
